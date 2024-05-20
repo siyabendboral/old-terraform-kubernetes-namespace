f52c6604371847771e27aa9f5a59d2033e925a46
siyabendboral <boralsiyabend@gmail.com>
2024-05-20 18:51:33 -0500

    added labels

--- variable.tf
+++ variable.tf
@@ -3,3 +3,14 @@ variable name {
   type = string
   default = ""
 }
+
+variable annotations {
+    description = "Please provide annotations"
+    type = map 
+    default = {}
+
+  variable labels 
+    description = "Please provide labels"
+    type = map 
+    default = {}
+  }
